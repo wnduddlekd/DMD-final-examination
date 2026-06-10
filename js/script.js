33fe403eac9ea85e549097e7386ed7bffb216478
wndud <ju03209@naver.com>
2026-06-10 21:18:28 +0900

    feat: 페이지 로딩 시에 컨테이너 애니메이션 표시되도록 수정

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 $(function () {
+  function checkScroll() {
+    $(".con").each(function () {
+      const top = $(this).offset().top;
+      const scrollTop = $(window).scrollTop();
+      const windowHeight = $(window).height();
+
+      if (scrollTop + windowHeight > top + 100) {
+        $(this).addClass("show");
+      }
+    });
+  }
+
+  $(window).on("scroll", checkScroll);
+
   function loadPage(page) {
     $("#content").fadeOut(200, function () {
       $(this).load(`/html/pages/${page}.html`, function () {
         $(this).fadeIn(200);
+        checkScroll();
       });
     });
   }
 
   function checkHash() {
     let hash = window.location.hash.replace("#", "");
-    if (hash === "") {
-      hash = "home";
-    }
+    if (hash === "") hash = "home";
     loadPage(hash);
   }
 
